2f7ee8418caaa200a68f7f5b4504392ad9f61911
Trevor D. Miller <trevordmiller@icloud.com>
2015-01-25 16:45:53 -0700

    Working prototype.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Sphero Music Shuffle
-Change your music randomly with Sphero collisions.
+# Sphero Music
+Play a new song from a Spotify URI when Sphero is tapped or collides with something in "randomRoll" mode.
 
 Using [cylon.js](http://cylonjs.com/documentation/drivers/sphero/)
 
@@ -8,10 +8,13 @@ Using [cylon.js](http://cylonjs.com/documentation/drivers/sphero/)
 ## Getting Started
 
 1. Install [Node](https://npmjs.org) if you haven't.
+1. Run `npm install -g spotify-cli` to get the Spotify CLI.
 1. Customize the `config` object.
     - Get `mySpheroPort` value by running `ls /dev/tty.Sphero*`.
+    - Get `spotifyUri` by right clicking on a Spotify artist, album, or playlist and selecting `Copy Spotify URI`.
+1. Open Spotify - it must be open for this app to work.
 1. Connect your Sphero via Bluetooth.
-    - It will only connect for a few seconds, so when it says `Connected` hurry and run `node app.js`.
+1. Run `node app.js` to start the app.
 
 ### Having problems?
 
--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 var Cylon = require('cylon');
 var chalk = require('chalk');
 var say = require('say');
+var sys = require('sys')
+var exec = require('child_process').exec;
 var meta = require('./package.json');
 
 // INTERNALS
 
+// Config
 var config = {
-    port: '/dev/tty.Sphero-WWB-AMP-SPP'
+    port: '/dev/tty.Sphero-WWB-AMP-SPP',
+    spotifyUri: 'spotify:artist:5oOhM2DFWab8XhSdQiITry',
+    randomRoll: false
 };
-var songs = [
-    'all-this-test.mp3',
-    'you-and-me.mp3'
-]
+
+// Shared functions
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
 
 // Init
 console.log(chalk.bold.magenta('----------------------------------------------------'));
@@ -20,43 +26,61 @@ console.log(chalk.bold.magenta('Running ' + meta.name + ' version ' + meta.versi
 console.log(chalk.bold.magenta('----------------------------------------------------'));
 
 Cylon.robot({
+
+    // Setup
     connections: {
         sphero: {
             adaptor: 'sphero',
             port: config.port
         }
     },
-
     devices: {
         sphero: {
             driver: 'sphero'
         }
     },
 
+    // Main
     work: function(me) {
-        var color = 0x00FF00,
-            bitFilter = 0xFFFF00;
+        var color = 0x800080,
+            songsPlayed = 0;
 
         // Init
         after((1).seconds(), function() {
-            say.speak('Alex', 'It\'s party time.')
+            say.speak('Alex', 'It\'s party time.');
             console.log('Setting up Collision Detection...');
             me.sphero.detectCollisions();
-            me.sphero.setRGB(color);
             me.sphero.stop();
+
+            // Start playing from Spotify URI
+            exec('spotify play ' + config.spotifyUri, puts);
+            console.log(chalk.bold.magenta('Playing chosen Spotify URI'));
+            color = 0x800080;
+            me.sphero.setRGB(color);
         });
 
-        // When a collision happens
+        // When a tap or collision happens
         me.sphero.on('collision', function() {
-            var newSong = songs[Math.floor(Math.random() * songs.length)];
-            console.log('Shuffle -> ' + newSong);
-            color = color ^ bitFilter;
-            me.sphero.setRGB(color);
+            songsPlayed = songsPlayed + 1;
+
+            // Skip to next song
+            exec('spotify next', puts);
+            if ((songsPlayed % 2) === 0) {
+                console.log(chalk.bold.magenta('Playing song ' + songsPlayed));
+                color = 0x800080;
+                me.sphero.setRGB(color);
+            } else {
+                console.log(chalk.bold.blue('Playing song ' + songsPlayed));
+                color= 0x0000FF;
+                me.sphero.setRGB(color);
+            }
         });
 
         // Randomly change direction every second
-        every((5).second(), function() {
-            me.sphero.roll(200, Math.floor(Math.random() * 360));
-        });
+        if(config.randomRoll === true) {
+            every((10).second(), function() {
+                me.sphero.roll(150, Math.floor(Math.random() * 360));
+            });
+        }
     }
 }).start();
